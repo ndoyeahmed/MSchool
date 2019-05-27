340a99168a4a1be5be57f12ac01257c5c5bdfe04
ndoyeahmed <ndoyeahmed2602@gmail.com>
2019-05-27 01:14:18 +0000

    fixed issues
    Signed-off-by: ndoyeahmed <ndoyeahmed2602@gmail.com>

--- app/src/main/java/controller/mouhamed/com/mschool/InscriptionActivity.java
+++ app/src/main/java/controller/mouhamed/com/mschool/InscriptionActivity.java
@@ -1,7 +1,7 @@
 package controller.mouhamed.com.mschool;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,11 +11,10 @@ import java.util.ArrayList;
 
 public class InscriptionActivity extends AppCompatActivity {
 
-    private EditText txtFirstName, txtLastName;
-    private Button btnSave;
+    /*private EditText txtFirstName, txtLastName;
     private CheckBox cbOLevel, cbBachelor, cbMaster;
-    private String firstName, lastName;
-    private String degrees;
+    private String firstName, lastName;*/
+//    private String degrees;
     private ArrayList<String> degreesListes = null;
 
     @Override
@@ -23,18 +22,20 @@ public class InscriptionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
 
-        txtFirstName = findViewById(R.id.txtFirstName);
-        txtLastName = findViewById(R.id.txtLastName);
-        btnSave = findViewById(R.id.btn_save);
-        cbBachelor = findViewById(R.id.cbBachelor);
-        cbMaster = findViewById(R.id.cbMaster);
-        cbOLevel = findViewById(R.id.cbOLevel);
+        final EditText txtFirstName = findViewById(R.id.txtFirstName);
+        final EditText txtLastName = findViewById(R.id.txtLastName);
+        final Button btnSave = findViewById(R.id.btn_save);
+        final CheckBox cbBachelor = findViewById(R.id.cbBachelor);
+        final CheckBox cbMaster = findViewById(R.id.cbMaster);
+        final CheckBox cbOLevel = findViewById(R.id.cbOLevel);
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                firstName = txtFirstName.getText().toString();
-                lastName = txtLastName.getText().toString();
+                String firstName = txtFirstName.getText().toString();
+                String lastName = txtLastName.getText().toString();
+                System.out.println(firstName);
+                System.out.println(lastName);
                 if (degreesListes != null) {
                     degreesListes.clear();
                 } else {
